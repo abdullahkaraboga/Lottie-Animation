ca1a2a527a0200206a06bdd138bc745ad91dfdb4
abdullahkaraboga <abdullahkaraboga@icloud.com>
2023-03-04 16:23:38 +0300

    Initial Commit

---
+++ LottieAnimation/LottieAnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LottieAnimationApp.swift
+//  LottieAnimation
+//
+//  Created by Abdullah Karaboğa on 4.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct LottieAnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
