3189b58d20ff2dd5a1ed39e30ce2d28c1d115984
abdullahkaraboga <abdullahkaraboga@icloud.com>
2023-03-04 17:30:02 +0300

    lottie package and animation added.

--- LottieAnimation/ContentView.swift
+++ LottieAnimation/ContentView.swift
@@ -6,16 +6,11 @@
 //
 
 import SwiftUI
+import Lottie
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
+       Home()
     }
 }
 
@@ -24,3 +19,75 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+struct Home: View {
+    var body: some View {
+
+        NavigationView {
+
+            NavigationLink(
+                destination:AnimationPage(),
+                label: {
+
+                    Label (title: {
+
+                        Text("Go to Animation Page")
+                            .font(.title)
+                            .fontWeight(.bold)
+
+                    }, icon: {
+
+                        Image(systemName: "arrow")
+                            .font(.system(size: 26,weight: .bold))
+                            .foregroundColor(.blue)
+
+                    })
+
+                })
+            .navigationTitle("Home")
+
+
+        }
+    }
+}
+
+struct AnimationPage : View{
+    
+    @State var black = Color.primary.opacity(0.7)
+    @Environment(\.presentationMode) var present
+    
+    var body: some View{
+       
+        VStack{
+            HStack{
+                
+                LottieView(animationName: "cat")
+                    .frame(width: 400, height: 400)
+               
+            }
+            .padding(.horizontal,50)
+        }
+        .navigationTitle("Animation")
+    }
+}
+
+struct LottieView : UIViewRepresentable {
+    
+    
+    
+    var animationName : String
+    
+    func makeUIView(context: Context) -> LottieAnimationView{
+        let view = LottieAnimationView(name: animationName,bundle: Bundle.main)
+        
+        view.loopMode = .loop
+        view.play()
+        
+        return view
+    }
+    
+    func updateUIView(_ uiView: Lottie.LottieAnimationView, context: Context) {
+    }
+    
+}
+
